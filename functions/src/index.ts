c8d94396093df38c86e9dee239f50eb3de6c74c1
Jennifer <jennifertan1994@gmail.com>
2023-12-07 10:31:17 -0500

    firebaseconfig

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,12 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+import accountRouter from "./routes/AccountRouter";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+const app = express();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+app.use(cors());
+app.use(express.json());
+app.use("/", accountRouter);
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const api = functions.https.onRequest(app);
